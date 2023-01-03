195fd72f2f56d282c08e60dadc481ee89fd631ac
Tetiana His <tania08082000@gmail.com>
2023-01-03 23:07:32 +0100

    search in staff members

--- private/controllers/Users.php
+++ private/controllers/Users.php
@@ -9,7 +9,15 @@ class Users extends Controller
         }
         $user = new User();
         $school_id = Auth::getSchool_id();
-        $users = $user->query("SELECT  * FROM users WHERE school_id = :school_id && rank not in ('student') ORDER BY id DESC ", ['school_id'=>$school_id]);
+        $query = "SELECT  * FROM users WHERE school_id = :school_id && rank not in ('student') ORDER BY id DESC ";
+        $arr = ['school_id'=>$school_id];
+
+        if(isset($_GET['search'])){
+            $find = '%' . $_GET['search'] . '%';
+            $query = "SELECT  * FROM users WHERE school_id = :school_id && (last_name like :lname || first_name like :fname) && rank not in ('student') ORDER BY id DESC ";
+            $arr = ['school_id'=>$school_id, 'fname' => $find, 'lname' => $find];
+        }
+        $users = $user->query($query, $arr);
         $crumbs[] = ['Dashboard', ROOT. '/'];
         $crumbs[] = ['Staff', ROOT. ''];
 
--- private/views/users.view.php
+++ private/views/users.view.php
@@ -5,10 +5,15 @@
 <div class="container container-fluid p-4 shadow mx-auto" style="max-width: 1000px">
     <?php $this->view('includes/breadcrumb', ['crumbs' => $crumbs]); ?>
     <div class="d-flex justify-content-between align-items-center">
-        <div class="input-group flex-nowrap mt-3 w-25 mb-3">
-            <span class="input-group-text" id="addon-wrapping"><i class="fa-solid fa-magnifying-glass"></i>&nbsp</span>
-            <input type="text" class="form-control" placeholder="Search" aria-label="Search" aria-describedby="addon-wrapping">
-        </div>
+        <form action="" method="get">
+            <div class="input-group flex-nowrap mt-3 mb-3">
+                <button type="submit" class="input-group-text" id="addon-wrapping"><i
+                            class="fa-solid fa-magnifying-glass"></i>&nbsp
+                </button>
+                <input value="<?=isset($_GET['search']) ? $_GET['search']: '';?>" name="search" type="text" class="form-control w-50" placeholder="Search" aria-label="Search"
+                       aria-describedby="addon-wrapping">
+            </div>
+        </form>
         <a href="<?= ROOT ?>/signup">
             <button class="btn btn-sm btn-primary"><i class="fa fa-plus me-2"></i>Add new</button>
         </a>
