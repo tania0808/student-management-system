6ff7f25d2790ce388fbe22a3e0116727bb15da7b
Tetiana His <tania08082000@gmail.com>
2023-01-03 23:00:34 +0100

    search in users

--- private/views/students.view.php
+++ private/views/students.view.php
@@ -5,10 +5,15 @@
 <div class="container container-fluid p-4 shadow mx-auto" style="max-width: 1000px">
     <?php $this->view('includes/breadcrumb', ['crumbs' => $crumbs]); ?>
     <div class="d-flex justify-content-between align-items-center">
-        <div class="input-group flex-nowrap mt-3 w-25 mb-3">
-            <span class="input-group-text" id="addon-wrapping"><i class="fa-solid fa-magnifying-glass"></i>&nbsp</span>
-            <input type="text" class="form-control" placeholder="Search" aria-label="Search" aria-describedby="addon-wrapping">
-        </div>
+        <form action="" method="get">
+            <div class="input-group flex-nowrap mt-3 mb-3">
+                <button type="submit" class="input-group-text" id="addon-wrapping"><i
+                            class="fa-solid fa-magnifying-glass"></i>&nbsp
+                </button>
+                <input value="<?=isset($_GET['search']) ? $_GET['search']: '';?>" name="search" type="text" class="form-control w-50" placeholder="Search" aria-label="Search"
+                       aria-describedby="addon-wrapping">
+            </div>
+        </form>
         <a href="<?= ROOT ?>/signup?mode=students">
             <button class="btn btn-sm btn-primary"><i class="fa fa-plus me-2"></i>Add new</button>
         </a>
@@ -16,12 +21,13 @@
 
     <div class="card-group justify-content-center">
         <?php
-        if($users){
+        if ($users) {
             foreach ($users as $user) { ?>
                 <?php include($this->views_path('user')); ?>
-            <?php }} else {?>
+            <?php }
+        } else { ?>
             <h4>No students were found at this time !</h4>
-        <?php }?>
+        <?php } ?>
     </div>
 </div>
 
